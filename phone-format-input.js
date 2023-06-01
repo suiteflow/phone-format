1439b84f28406d874ce8af54d8d9613c4406693c
Aaron Grieve <127838213+suiteflow@users.noreply.github.com>
2023-06-01 11:47:38 +0100

    Update phone-format-input.js

--- phone-format-input.js
+++ phone-format-input.js
@@ -2,42 +2,45 @@ document.addEventListener("DOMContentLoaded", function() {
     var phoneInputs = document.querySelectorAll('[suite-input="phone"]');
 
     phoneInputs.forEach(function(phoneInput) {
+        var format = phoneInput.getAttribute('suite-format');
+        var countryCode;
+        if (format === 'us') {
+            countryCode = '1';
+        } else if (format === 'uk') {
+            countryCode = '44';
+        } else if (format === 'kw') {
+            countryCode = '965';
+        }
+
+        // Prepend the country code when the page loads
+        if (phoneInput.value.length === 0) {
+            phoneInput.value = '+' + countryCode + ' ';
+        }
+
         phoneInput.addEventListener('input', function() {
             var input = this.value;
-            // remove any non-digit characters
-            input = input.replace(/\D/g, '');
+            // remove any non-digit characters except for the country code
+            input = input.replace(new RegExp('(?<!^\\+' + countryCode + ')\\D', 'g'), '');
 
-            var format = this.getAttribute('suite-format');
-
-            if (input.length > 0) {
+            if (input.length > countryCode.length + 1) {
                 if (format === 'us') {
-                    // Check if input already starts with '1', if not prepend it
-                    if(input.substr(0, 1) !== '1') {
-                        input = '1' + input;
-                    }
                     // format the input (XXX) XXX-XXXX for US
-                    if (input.length >= 11) {
+                    if (input.length >= countryCode.length + 10) {
                         input = '+' + input.substr(0, 1) + ' (' + input.substr(1, 3) + ') ' + input.substr(4, 3) + '-' + input.substr(7, 4);
                     }
                 } else if (format === 'uk') {
-                    // Check if input already starts with '44', if not prepend it
-                    if(input.substr(0, 2) !== '44') {
-                        input = '44' + input;
-                    }
                     // format the input XXXXX XXXXXX for UK
-                    if (input.length >= 12) {
+                    if (input.length >= countryCode.length + 11) {
                         input = '+' + input.substr(0, 2) + ' ' + input.substr(2, 5) + ' ' + input.substr(7);
                     }
                 } else if (format === 'kw') {
-                    // Check if input already starts with '965', if not prepend it
-                    if(input.substr(0, 3) !== '965') {
-                        input = '965' + input;
-                    }
                     // format the input XXXX XXXX for Kuwait
-                    if (input.length >= 11) {
+                    if (input.length >= countryCode.length + 9) {
                         input = '+' + input.substr(0, 3) + ' ' + input.substr(3, 4) + ' ' + input.substr(7, 4);
                     }
                 }
+            } else {
+                input = '+' + countryCode + ' ';
             }
 
             // set the input value to the formatted input
